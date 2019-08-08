b24a9369d0e6d4b189e3ca8d2bfef53469b778c9
egemenyavuz <melih.egemen.yavuz@sysco.no>
2019-08-08 09:57:11 +0200

    implemented transit decoding adopted to sesamutils updated readme

--- README.md
+++ README.md
@@ -1,47 +1,64 @@
-# dynamic-url-proxy
-Microservice for generating dynamic urls
+[![Build Status](https://travis-ci.org/sesam-community/delayed-rest-proxy.svg?branch=master)](https://travis-ci.org/sesam-community/delayed-rest-proxy)
 
-This microservice can be used to
-  * dynamically build url based on some function evaluation
 
-Main idea behind is that filenames or urls might have -for instance- datetime related fix in their names. With this microservice you can build a url in "replace substring" manner and forward the call as if you are calling it directly.
+# delayed-rest-proxy
+Microservice for breaking down the rest-sink batch into single items and sending each with a delay in between
 
-#How-TO
-For every dynamic part in your url:
-  * replace it by [<part_name>]
-  * add a query parameter <part_name>=<python_function_to_be_evaluated>
+ ### Environment Parameters
 
-It is only datetime module that are made available to 'eval' additonally to builtins module. You will have to customize after your needs, if any.
+ | CONFIG_NAME        | DESCRIPTION           | IS_REQUIRED  |DEFAULT_VALUE|
+ | -------------------|---------------------|:------------:|:-----------:|
+ | OPERATIONS | similar to 'operations' property in [Sesam's built-in REST system](https://docs.sesam.io/configuration.html#the-rest-system) with only 'url', 'method', 'headers' properties | yes | n/a |
+ | URL_PATTERN |  same as 'url_pattern' in  in [Sesam's built-in REST system](https://docs.sesam.io/configuration.html#the-rest-system)  Azure Storage account key. | no, basic auth alternatively | n/a |
+ | DELAY_DURATION_IN_SECONDS | number of seconds to wait between each rest request  | no | 60 |
+ | LOG_LEVEL | log level. One of DEBUG,INFO,WARNING,ERROR,CRITICAL | no | INFO |
 
-### example config in SESAM
-system:
-```
-{
-    "_id": "my-dynamic-url-proxy",
-    "type": "system:microservice",
-    "connect_timeout": 60,
-    "docker": {
-        "environment": {
-            "LOGLEVEL": "DEBUG",
-            "PORT": 5000
-        },
-        "image": "sesamcommunity/dynamic-url-prox:v1.0",
-        "port": 5000
-    },
-    "read_timeout": 7200
-}
 
-```
-pipe:
-```
-...
-...
-,
-    "source": {
-        "type": "json",
-        "system": "my-dynamic-url-proxy",
-        "url": "/?url=http://my-server:5000/my_file-[replace_me].csv&replace_me=datetime.datetime.now().replace(day=1).strftime('%Y%m%d')"
-    },
-...
-...
-```
+ ### Query Parameters
+
+ None for the microservice. Any query string that is sent to the microservice will be forwarded to the target system
+
+
+
+ ### An example of system config:
+
+ ```json
+ {
+   "_id": "my-delayed-rest-system",
+   "type": "system:microservice",
+   "docker": {
+     "environment": {
+       "DELAY_DURATION_IN_SECONDS": "3",
+       "LOG_LEVEL": "DEBUG",
+       "OPERATIONS": {
+         "put-customer": {
+           "headers": {
+             "Content-type": "application/json; charset=utf-8"
+           },
+           "method": "PUT",
+           "url": "customer"
+         },
+         "post-customer": {
+           "headers": {
+             "Content-type": "application/json; charset=utf-8"
+           },
+           "method": "POST",
+           "url": "customer"
+         },
+         "delete-customer": {
+           "headers": {
+             "Content-type": "application/json; charset=utf-8"
+           },
+           "method": "DELETE",
+           "url": "customer"
+         }
+       },
+       "URL_PATTERN": "http://my-customer-system/%s"
+     },
+     "image": "sesamcommunity/delayed-rest-proxy:9.9.9",
+     "memory": 512,
+     "port": 5000
+   }
+ }
+
+ ```
--- service/proxy-service.py
+++ service/proxy-service.py
@@ -3,74 +3,88 @@ from flask import Flask, request, Response
 import os
 import json
 import datetime
-import logger
+import re
 from time import sleep
+from sesamutils import VariablesConfig, sesam_logger
+from sesamutils.flask import serve
+import sys
+from werkzeug.routing import Rule
 
 app = Flask(__name__)
+app.url_map.add(Rule('/', endpoint='proxy'))
 
-logger = logger.Logger("delayed-rest-proxy", os.environ.get(
-    "LOGLEVEL", "INFO"))
+required_env_vars = ["OPERATIONS", "URL_PATTERN"]
+optional_env_vars = [("DELAY_DURATION_IN_SECONDS", 60), "LOG_LEVEL"]
+env_config = VariablesConfig(
+    required_env_vars, optional_env_vars=optional_env_vars)
 
-DELAY_DURATION_IN_SECONDS = int(
-    os.environ.get("DELAY_DURATION_IN_SECONDS", 60))
+if not env_config.validate():
+    sys.exit(1)
 
-OPERATIONS = json.loads(os.environ.get("OPERATIONS"))
-URL_PATTERN = os.environ.get("URL_PATTERN")
+env_config.OPERATIONS = json.loads(env_config.OPERATIONS)
+env_config.DELAY_DURATION_IN_SECONDS = int(env_config.DELAY_DURATION_IN_SECONDS)
 
+logger = sesam_logger("delayed-rest-proxy", app=app)
 logger.info(
-    "starting service with URL_PATTERN=%s, DELAY_DURATION_IN_SECONDS=%d, OPERATIONS=%s"
-    % (URL_PATTERN, DELAY_DURATION_IN_SECONDS, OPERATIONS))
-methods = [
-    "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "CONNECT", "PATCH",
-    "TRACE"
-]
+    "starting service with \n\tURL_PATTERN=%s\n\tOPERATIONS=%s, \n\tDELAY_DURATION_IN_SECONDS=%d, \n\tLOG_LEVEL=%s"
+    % (env_config.URL_PATTERN, env_config.OPERATIONS, env_config.DELAY_DURATION_IN_SECONDS, logger.level))
 
 
-def generate_response(response_text, status_code, content_type):
-    return Response(
-        response=response_text, status=status_code, content_type=content_type)
+def generate_response(status_code, message):
+    return Response(response=json.dumps({
+        "is_success": (status_code >= 200 and status_code < 300),
+        "message": message
+    }), status=status_code, content_type="application/json")
 
 
-@app.route("/", methods=methods)
+def transit_decode(payload):
+    if isinstance(payload, dict):
+        transit_decoded_payload = {}
+        for key, value in payload.items():
+            transit_decoded_payload[key] = transit_decode(value)
+    elif isinstance(payload, list):
+        transit_decoded_payload = []
+        for item in payload:
+            transit_decoded_payload.append(transit_decode(item))
+    else:
+        transit_decoded_payload = None
+        if payload and isinstance(payload, str):
+            transit_decoded_payload = re.sub(r'~[:rtbuf]', '', payload)
+        else:
+            transit_decoded_payload = payload
+    return transit_decoded_payload
+
+@app.endpoint('proxy')
 def proxy(path=""):
     try:
         is_first = True
         s = requests.Session()
         s.verify = False
-        for entity in request.get_json():
+        input_json = request.get_json()
+        if isinstance(input_json, dict):
+            input_json = [input_json]
+        for entity in input_json:
+            transfer_decoded_entity=transit_decode(entity)
             if not is_first:
-                sleep(DELAY_DURATION_IN_SECONDS)
+                sleep(env_config.DELAY_DURATION_IN_SECONDS)
             is_first = False
-            operation = OPERATIONS.get(entity.get("operation"))
-            payload = entity.get("payload")
-            logger.debug((URL_PATTERN % operation.get("url")) +
-                         "Handling operation=%s, payload=%s" %
-                         (operation, payload))
+            operation = env_config.OPERATIONS.get(entity.get("operation"))
+            payload = transit_decode(entity.get("payload"))
+            logger.debug("sending {} request for _id={} to url={}".format(operation.get("method"), transfer_decoded_entity.get("_id", "-"), (env_config.URL_PATTERN % operation.get("url"))))
+
             r = s.request(
                 method=operation.get("method"),
-                url=(URL_PATTERN % operation.get("url")),
+                url=(env_config.URL_PATTERN % operation.get("url")),
                 headers=operation.get("headers"),
                 params=request.args.items(),
                 json=payload)
 
-        return generate_response(
-            json.dumps({
-                "is_success": True,
-                "message": ""
-            }), 200, "application/json")
+        return generate_response(200, "")
 
     except Exception as e:
         logger.exception(e)
-        return generate_response(
-            json.dumps({
-                "is_success": False,
-                "message": str(e)
-            }), 500, "application/json")
+        return generate_response(500, str(e))
 
 
 if __name__ == '__main__':
-    app.run(
-        threaded=True,
-        debug=True,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)))
+    serve(app)
